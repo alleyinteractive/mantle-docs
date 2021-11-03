92480221fe8fd19847a567691ab77fd7fd27f19a
Sean Fisher <srtfisher@gmail.com>
2021-11-03 09:55:33 -0400

    Adding favicons

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -9,6 +9,11 @@ module.exports = {
     ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
     ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
     ['meta', { property: 'og:image', content: 'https://repository-images.githubusercontent.com/261240189/e61bc280-2d73-11eb-92d0-249447854ca0' }],
+
+    ['link', { rel: 'apple-touch-icon', type: 'image/png', sizes: '180x180', href: '/apple-touch-icon.png' }],
+    ['link', { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' }],
+    ['link', { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' }],
+    ['meta', { name: 'theme-color', content: '#234568' }],
   ],
   themeConfig: {
     activeHeaderLinks: false,
