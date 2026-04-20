42ff866314c540217156844e855db7c7e8e3e0e0
Sean Fisher <srtfisher@gmail.com>
2026-04-20 13:07:29 -0400

    Add markdown alternate link to doc pages
    Swizzles DocItem/Layout to inject <link rel="alternate" type="text/markdown">
    pointing to the hosted .md version so agents can discover it.

---
+++ src/theme/DocItem/Layout/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import Head from '@docusaurus/Head';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import { useDoc } from '@docusaurus/plugin-content-docs/client';
+import Layout from '@theme-original/DocItem/Layout';
+import type LayoutType from '@theme/DocItem/Layout';
+import type { WrapperProps } from '@docusaurus/types';
+
+type Props = WrapperProps<typeof LayoutType>;
+
+export default function LayoutWrapper(props: Props): JSX.Element {
+  const { metadata } = useDoc();
+  const { siteConfig } = useDocusaurusContext();
+  const markdownUrl = new URL(
+    `${metadata.permalink}.md`,
+    siteConfig.url,
+  ).toString();
+
+  return (
+    <>
+      <Head>
+        <link rel="alternate" type="text/markdown" href={markdownUrl} />
+      </Head>
+      <Layout {...props} />
+    </>
+  );
+}
